abcf3a56a6f6e99af90594e22b0cbd244b8e0a23
Bhushan Shah <bhush94@gmail.com>
2021-06-09 20:20:21 +0530

    various changes related to versioning
    - use PROJECT_VERSION for allowing scripting
    - Add version option to command line parser
    - Drop unused cmake code

--- src/main.cpp
+++ src/main.cpp
@@ -88,6 +88,7 @@ int main(int argc, char *argv[])
 
     QCommandLineParser parser;
     parser.setApplicationDescription(i18n("Client for the decentralized social network: mastodon"));
+    parser.addVersionOption();
 
     about.setupCommandLine(&parser);
     parser.process(app);
