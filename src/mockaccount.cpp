936a3ffadaa8b8803a47a52405df4ba7ba9e6d7e
Carl Schwan <carl@carlschwan.eu>
2022-11-30 23:55:24 +0100

    Fix reuse
    Signed-off-by: Carl Schwan <carl@carlschwan.eu>

--- src/mockaccount.cpp
+++ src/mockaccount.cpp
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: 2021 Carl Schwan <carl@carlschwan.eu>
+// SPDX-License-Identifier: GPL-2.0-or-later
+
 #include "mockaccount.h"
 
 MockAccount::MockAccount(QObject *parent)
