53a11b9dfac04120fc086e9fb7834ae803609322
Joshua Goins <josh@redstrate.com>
2023-01-03 11:44:47 -0500

    Fix blurhash greedy decoding
    QUrl::fromPercentEncoding destroys some valid base83 strings that
    contain encodings like %14, which Qt turns into Unicode garbage. Now, we
    use our own encoding table containing the only special ASCII characters
    that blurhash uses.

--- src/blurhashimageprovider.cpp
+++ src/blurhashimageprovider.cpp
@@ -13,6 +13,35 @@ BlurhashImageProvider::BlurhashImageProvider()
 {
 }
 
+/*
+ * Qt unfortunately re-encodes the base83 string in QML.
+ * The only special ASCII characters used in the blurhash base83 string are:
+ * #$%*+,-.:;=?@[]^_{|}~
+ * QUrl::fromPercentEncoding is too greedy, and spits out invalid characters
+ * for parts of valid base83 like %14.
+ */
+static const QMap<QLatin1String, QLatin1String> knownEncodings = {
+        {QLatin1String("%23A"), QLatin1String(":")},
+        {QLatin1String("%3F"), QLatin1String("?")},
+        {QLatin1String("%23"), QLatin1String("#")},
+        {QLatin1String("%5B"), QLatin1String("[")},
+        {QLatin1String("%5D"), QLatin1String("]")},
+        {QLatin1String("%40"), QLatin1String("@")},
+        {QLatin1String("%24"), QLatin1String("$")},
+        {QLatin1String("%2A"), QLatin1String("*")},
+        {QLatin1String("%2B"), QLatin1String("+")},
+        {QLatin1String("%2C"), QLatin1String(",")},
+        {QLatin1String("%2D"), QLatin1String("-")},
+        {QLatin1String("%2E"), QLatin1String(".")},
+        {QLatin1String("%3B"), QLatin1String(";")},
+        {QLatin1String("%3D"), QLatin1String("=")},
+        {QLatin1String("%25"), QLatin1String("%")},
+        {QLatin1String("%5E"), QLatin1String("^")},
+        {QLatin1String("%7C"), QLatin1String("|")},
+        {QLatin1String("%7B"), QLatin1String("{")},
+        {QLatin1String("%7D"), QLatin1String("}")},
+};
+
 QImage BlurhashImageProvider::requestImage(const QString &id, QSize *size, const QSize &requestedSize)
 {
     if (id.isEmpty()) {
@@ -25,7 +54,14 @@ QImage BlurhashImageProvider::requestImage(const QString &id, QSize *size, const
     if (size->height() == -1) {
         size->setHeight(256);
     }
-    auto data = decode(QUrl::fromPercentEncoding(id.toLatin1()).toLatin1().data(), size->width(), size->height(), 1, 3);
+
+    QString decodedId = id;
+
+    QMap<QLatin1String, QLatin1String>::const_iterator i;
+    for (i = knownEncodings.constBegin(); i != knownEncodings.constEnd(); ++i)
+        decodedId.replace(i.key(), i.value());
+
+    auto data = decode(decodedId.toLatin1().constData(), size->width(), size->height(), 1, 3);
     QImage image(data, size->width(), size->height(), size->width() * 3, QImage::Format_RGB888, free, data);
     return image;
 }
