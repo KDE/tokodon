502f1bf1bd43d31c40fb4dd54ae6090c7681a79b
Joshua Goins <josh@redstrate.com>
2023-05-21 08:28:19 -0400

    Add SearchModel documentation

--- src/searchmodel.h
+++ src/searchmodel.h
@@ -8,6 +8,8 @@
 class Identity;
 class Post;
 
+/// Represents a search hashtag result, which only stores the name (and skips other information like occurences) for now
+/// \see SearchModel
 class SearchHashtag
 {
 public:
@@ -19,33 +21,48 @@ private:
     QString m_name;
 };
 
+/// Model used to fetch search results
+/// \see AbstractTimelineModel
 class SearchModel : public AbstractTimelineModel
 {
     Q_OBJECT
     Q_PROPERTY(bool loaded READ loaded NOTIFY loadedChanged)
 
 public:
+    /// The search result type
     enum ResultType {
-        Status,
-        Account,
-        Hashtag,
+        Status, ///< A status (for full-text search)
+        Account, ///< An account
+        Hashtag, ///< A hashtag
     };
     Q_ENUM(ResultType);
 
     explicit SearchModel(QObject *parent = nullptr);
     ~SearchModel();
 
+    /// Check if the search has finished loading
+    /// \see setLoaded
     bool loaded() const;
+
+    /// Sets the search loading status
+    /// \see laoded
     void setLoaded(bool loaded);
 
+    /// Start searching for \p queryString
     Q_INVOKABLE void search(const QString &queryString);
+
+    /// Get a localized label for a result type
     Q_INVOKABLE QString labelForType(SearchModel::ResultType sectionType);
+
+    /// Clear the fetched search results
     Q_INVOKABLE void clear();
 
     int rowCount(const QModelIndex &parent) const override;
     QVariant data(const QModelIndex &index, int role) const override;
 
 Q_SIGNALS:
+    /// Emitted if the loading status has changed
+    /// \see setLoaded
     void loadedChanged();
 
 private:
