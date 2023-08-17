cb4c723e76eebc3f49735bc6d3fe1f7427bfe1c3
Carl Schwan <carl@carlschwan.eu>
2023-08-17 22:46:24 +0200

    Fix typo in license
    GPL-2.0-or-later OR GPL-3.0-only OR LicenseRef-KDE-Accepted-LGPL doesn't
    make any sense and is equivalent to just GPL-2.0-or-later

--- src/network/networkcontroller.cpp
+++ src/network/networkcontroller.cpp
@@ -1,5 +1,5 @@
 // SPDX-FileCopyrightText: 2022 Gary Wang <wzc782970009@gmail.com>
-// SPDX-License-Identifier: GPL-2.0-or-later OR GPL-3.0-only OR LicenseRef-KDE-Accepted-LGPL
+// SPDX-License-Identifier: GPL-2.0-only OR GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL
 
 #include "networkcontroller.h"
 
--- src/utils/androidutils.cpp
+++ src/utils/androidutils.cpp
@@ -1,5 +1,5 @@
 // SPDX-FileCopyrightText: 2023 Joshua Goins <josh@redstrate.com>
-// SPDX-License-Identifier: GPL-2.0-or-later OR GPL-3.0-only OR LicenseRef-KDE-Accepted-LGPL
+// SPDX-License-Identifier: GPL-2.0-only OR GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL
 
 #include "androidutils.h"
 
