dd6df4203407f2acf1039b7bf589974a48a463c3
Joshua Goins <josh@redstrate.com>
2023-09-30 00:06:46 -0400

    NotificationHandler: Add basic documentation

--- src/account/notificationhandler.h
+++ src/account/notificationhandler.h
@@ -9,12 +9,17 @@
 
 class QNetworkAccessManager;
 
+/// Handles desktop notifications using KNotification
 class NotificationHandler : public QObject
 {
     Q_OBJECT
 
 public:
     explicit NotificationHandler(QNetworkAccessManager *nam, QObject *parent = nullptr);
+
+    /// Display a new notification for an account
+    /// \param notification The shared handle for the notification
+    /// \param account The account the notification belongs to
     void handle(std::shared_ptr<Notification> notification, AbstractAccount *account);
 
 private:
