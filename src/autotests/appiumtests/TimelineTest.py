f590c18fcccbf3c90d150d694ef59eb5b97d4416
Joshua Goins <josh@redstrate.com>
2024-03-19 22:39:21 -0400

    More TimelineTest fixes

--- src/autotests/appiumtests/TimelineTest.py
+++ src/autotests/appiumtests/TimelineTest.py
@@ -11,6 +11,8 @@ from appium.options.common.base import AppiumOptions
 from appium.webdriver.common.appiumby import AppiumBy
 from appium.options.common.app_option import AppOption
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 class ATSPIOptions(AppiumOptions, AppOption):
@@ -22,6 +24,7 @@ class TimelineTest(unittest.TestCase):
     def setUp(self):
         options = ATSPIOptions()
         options.app = tokodon_offline_path
+        options.set_capability("timeouts", {'implicit': 10000})
         self.driver = webdriver.Remote(
             command_executor='http://127.0.0.1:4723',
             options=options)
@@ -53,8 +56,14 @@ class TimelineTest(unittest.TestCase):
         timelineElement = self.driver.find_element(AppiumBy.CLASS_NAME, "[page tab | Home]")
         timelineElement.send_keys(Keys.DOWN)
         timelineElement.send_keys(Keys.DOWN)
-        timelineElement.send_keys(Keys.DOWN)
+        time.sleep(2)
+
         self.assertTrue(self.driver.find_element(by='description', value="Status with image attachment"))
+
+        timelineElement.send_keys(Keys.DOWN)
+        timelineElement.send_keys(Keys.DOWN)
+        time.sleep(2)
+
         self.assertTrue(self.driver.find_element(by='description', value="Status with Video attachment"))
         self.assertTrue(self.driver.find_element(by='description', value="Status with GifV attachment"))
 
