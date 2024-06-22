6327833567ca0cadf027818c7e56763bf778586c
Joshua Goins <josh@redstrate.com>
2024-06-22 12:44:20 -0400

    Document more of Navigation

--- src/utils/navigation.h
+++ src/utils/navigation.h
@@ -8,6 +8,9 @@
 #include "account/identity.h"
 #include "timeline/post.h"
 
+/**
+ * @brief Deals with navigation in the QML pages but also allows C++ to control some of the navigation too.
+ */
 class Navigation : public QObject
 {
     Q_OBJECT
@@ -35,14 +38,56 @@ public:
     Q_INVOKABLE bool hasWebView() const;
 
 Q_SIGNALS:
+    /**
+     * @brief Opens the composer UI with @p text.
+     */
     void openComposer(const QString &text);
+
+    /**
+     * @brief Opens the composer UI to reply to @p post.
+     */
     void replyTo(Post *post);
+
+    /**
+     * @brief Opens the profile page for @p accountId.
+     */
     void openAccount(const QString &accountId);
+
+    /**
+     * @brief Opens the thread page for @p postId.
+     */
     void openPost(const QString &postId);
+
+    /**
+     * @brief Opens the fullscreen image viewer.
+     * @param attachments List of attachments.
+     * @param identity The identity of the post author.
+     * @param currentIndex The index of the attachment to open on.
+     */
     void openFullScreenImage(const QVariant &attachments, Identity *identity, int currentIndex);
+
+    /**
+     * @brief Opens the tag page for @p tag.
+     */
     void openTag(const QString &tag);
+
+    /**
+     * @brief Opens the report dialog.
+     * @param identity The identity of the post author.
+     * @param postId The post's id.
+     */
     void reportPost(Identity *identity, const QString &postId);
+
+    /**
+     * @brief Opens the report dialog for the account of @p identity.
+     */
     void reportUser(Identity *identity);
+
+    /**
+     * @brief Opens the list page.
+     * @param listId The list id.
+     * @param listName The name assigned to the list.
+     */
     void openList(const QString &listId, const QString &listName);
 
 private:
