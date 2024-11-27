513fbee8652f67fe725be282171b362f83633235
Carl Schwan <carl@carlschwan.eu>
2024-11-28 00:27:48 +0100

    Remove myself from copyright
    I am already in the author list

--- src/main.cpp
+++ src/main.cpp
@@ -114,7 +114,7 @@ int main(int argc, char *argv[])
                      QStringLiteral(TOKODON_VERSION_STRING),
                      i18n("Browse the Fediverse"),
                      KAboutLicense::GPL_V3,
-                     i18n("© 2021-2024 Carl Schwan, 2021-2024 KDE Community"));
+                     i18n("© 2021-2024 KDE Community"));
     about.addAuthor(i18n("Carl Schwan"),
                     i18n("Maintainer"),
                     QStringLiteral("carl@carlschwan.eu"),
