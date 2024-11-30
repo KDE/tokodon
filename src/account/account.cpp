67bb62559fde8c8a5cfef595f15e6011c6555f71
Joshua Goins <josh@redstrate.com>
2024-11-30 14:07:46 -0500

    Simplify the AbstractAccount/Account contructors, remove unused stuff
    Ignore SSL errors is a no longer accessible option, and can be removed.
    The admin parameter is still relevant, and is moved to registerTokodon
    instead.
    
    (cherry picked from commit e9610931fefaf35a958972a967f6d9ec2e5b3b47)

--- src/account/account.cpp
+++ src/account/account.cpp
@@ -20,14 +20,12 @@
 
 using namespace Qt::Literals::StringLiterals;
 
-Account::Account(const QString &instanceUri, QNetworkAccessManager *nam, bool ignoreSslErrors, bool admin, QObject *parent)
+Account::Account(const QString &instanceUri, QNetworkAccessManager *nam, QObject *parent)
     : AbstractAccount(parent, instanceUri)
-    , m_ignoreSslErrors(ignoreSslErrors)
     , m_qnam(nam)
 {
     m_preferences = new Preferences(this);
     setInstanceUri(instanceUri);
-    m_requestingAdmin = admin;
 }
 
 Account::Account(AccountConfig *settings, QNetworkAccessManager *nam, QObject *parent)
@@ -190,11 +188,6 @@ void Account::handleReply(QNetworkReply *reply, std::function<void(QNetworkReply
             reply_cb(reply);
         }
     });
-    if (m_ignoreSslErrors) {
-        connect(reply, &QNetworkReply::sslErrors, this, [reply](const QList<QSslError> &) {
-            reply->ignoreSslErrors();
-        });
-    }
 }
 
 // assumes file is already opened and named
@@ -376,7 +369,6 @@ void Account::writeToSettings()
     config.setClientId(m_client_id);
     config.setInstanceUri(m_instance_uri);
     config.setName(m_name);
-    config.setIgnoreSslErrors(m_ignoreSslErrors);
 
     config.save();
 
@@ -402,7 +394,6 @@ void Account::buildFromSettings()
     m_client_id = m_config->clientId();
     m_name = m_config->name();
     m_instance_uri = m_config->instanceUri();
-    m_ignoreSslErrors = m_config->ignoreSslErrors();
 
     auto accessTokenJob = new QKeychain::ReadPasswordJob{QStringLiteral("Tokodon"), this};
 #ifdef SAILFISHOS
@@ -553,11 +544,11 @@ QUrlQuery Account::buildNotificationFormData()
     return formdata;
 }
 
-void Account::registerTokodon(const bool authCode)
+void Account::registerTokodon(const bool authCode, const bool admin)
 {
     registerApplication(QStringLiteral("Tokodon"),
                         QStringLiteral("https://apps.kde.org/tokodon"),
-                        m_requestingAdmin ? QStringLiteral("admin:read admin:write") : QStringLiteral(""),
+                        admin ? QStringLiteral("admin:read admin:write") : QStringLiteral(""),
                         authCode);
 }
 
