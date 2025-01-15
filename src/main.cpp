848128cf6a0ebf0435c3c64a2742d08cff1396b8
Joshua Goins <josh@redstrate.com>
2025-01-15 17:48:29 -0500

    Explicitly include QCommandLineParser

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 // SPDX-FileCopyrightText: 2021 Carl Schwan <carl@carlschwan.eu>
 // SPDX-License-Identifier: GPL-2.0-only OR GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL
 
+#include <QCommandLineParser>
 #include <QQmlApplicationEngine>
 #include <QQuickStyle>
 #include <QQuickWindow>
