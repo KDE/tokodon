KDE/tokodon

commit c75a40e869c8295006cc69544762a5300d431b8d

Add follow request notifications, and also allow unknown ones
I noticed when we encounter unknown notification types it kills the
whole notification page (due to QML shenanigans, I imagine.) So I added
a small little fallback, which isn't ideal but its better than your
notifications not working at all!

There wasn't a delegate for follow requests apparently, so I added one.

I also adjusted the padding on the other notification delegates so they
match now!

src/content/ui/NotificationPage.qml
@@ -20,7 +20,7 @@ Kirigami.ScrollablePage {
     property var dialog: null
 
     property alias listViewHeader: listview.header
-    readonly property bool typesAreGroupable: showAllAction.checked
+    readonly property bool typesAreGroupable: notificationModel.excludeTypes === []
     property bool shouldGroupNotifications: typesAreGroupable
     readonly property var currentModel: shouldGroupNotifications ? groupedNotificationModel : notificationModel
 
@@ -243,6 +243,11 @@ Kirigami.ScrollablePage {
             }
 
             DelegateChoice {
+                roleValue: Notification.FollowRequest
+                FollowDelegate {}
+            }
+
+            DelegateChoice {
                 roleValue: Notification.Update
                 PostDelegate {
                     width: ListView.view.width
@@ -258,11 +263,10 @@ Kirigami.ScrollablePage {
                 roleValue: Notification.Status
                 PostDelegate {
                     width: ListView.view.width
-                    secondary: true
+                    secondary: false
                     timelineModel: groupedNotificationModel
                     loading: listview.model.loading
                     showSeparator: index !== ListView.view.count - 1
-                    showInteractionButton: false
                 }
             }
 
@@ -301,6 +305,11 @@ Kirigami.ScrollablePage {
             DelegateChoice {
                 roleValue: Notification.AnnualReport
                 AnnualReportDelegate {}
+            }
+
+            // If we don't have this, any unknown notification will prevent loading the entire notification page
+            DelegateChoice {
+                UnknownNotificationDelegate {}
             }
         }
 

src/CMakeLists.txt
@@ -262,6 +262,8 @@ qt_target_qml_sources(tokodon_static
         content/ui/Notifications/ReportDelegate.qml
         content/ui/Notifications/UserInteractionLabel.qml
         content/ui/Notifications/AnnualReportDelegate.qml
+        content/ui/Notifications/FollowDelegate.qml
+        content/ui/Notifications/UnknownNotificationDelegate.qml
 
         # Settings
         content/ui/Settings/AccountPage.qml
@@ -317,7 +319,6 @@ qt_target_qml_sources(tokodon_static
         content/ui/ConversationPage.qml
         content/ui/EditListPage.qml
         content/ui/ExplorePage.qml
-        content/ui/FollowDelegate.qml
         content/ui/FollowingPage.qml
         content/ui/FullScreenImage.qml
         content/ui/LanguageSelector.qml

src/content/ui/Notifications/AccountWarningDelegate.qml
@@ -47,6 +47,7 @@ QQC2.ItemDelegate {
 
         maximumWidth: Kirigami.Units.gridUnit * 40
         spacing: 0
+        padding: 0
 
         RowLayout {
             spacing: Kirigami.Units.smallSpacing

src/content/ui/Notifications/AnnualReportDelegate.qml
@@ -46,6 +46,7 @@ QQC2.ItemDelegate {
 
         maximumWidth: Kirigami.Units.gridUnit * 40
         spacing: 0
+        padding: 0
 
         RowLayout {
             spacing: Kirigami.Units.largeSpacing

src/content/ui/Notifications/FollowDelegate.qml
@@ -6,10 +6,11 @@ import QtQuick.Controls 2 as QQC2
 import QtQuick.Layouts
 import Qt.labs.qmlmodels 1.0
 import org.kde.kirigami 2 as Kirigami
+import org.kde.kirigamiaddons.statefulapp as StatefulApp
 import org.kde.tokodon
 
-import "./PostDelegate"
-import "./Components"
+import "../PostDelegate"
+import "../Components"
 
 QQC2.ItemDelegate {
     id: root
@@ -21,11 +22,18 @@ QQC2.ItemDelegate {
 
     required property var type
     readonly property bool isAdminSignUp: type === Notification.AdminSignUp
+    readonly property bool isFollowRequest: type === Notification.FollowRequest
 
-    topPadding: Kirigami.Units.smallSpacing
-    bottomPadding: Kirigami.Units.largeSpacing * 2
-    leftPadding: Kirigami.Units.largeSpacing * 2
-    rightPadding: Kirigami.Units.largeSpacing * 2
+    padding: 0
+    topPadding: Kirigami.Units.largeSpacing
+    bottomPadding: Kirigami.Units.largeSpacing
+    leftPadding: Kirigami.Units.largeSpacing
+    rightPadding: Kirigami.Units.largeSpacing
+
+    topInset: 0
+    leftInset: 0
+    rightInset: 0
+    bottomInset: 0
 
     highlighted: false
     hoverEnabled: false
@@ -49,7 +57,8 @@ QQC2.ItemDelegate {
         id: flexColumn
 
         maximumWidth: Kirigami.Units.gridUnit * 40
-        spacing: 0
+        spacing: Kirigami.Units.largeSpacing
+        padding: 0
 
         RowLayout {
             spacing: Kirigami.Units.smallSpacing
@@ -65,8 +74,10 @@ QQC2.ItemDelegate {
 
             QQC2.Label {
                 font: Config.defaultFont
-                text: if (isAdminSignUp) {
+                text: if (root.isAdminSignUp) {
                     i18n("%1 signed up", root.notificationActorIdentity.displayNameHtml)
+                } else if(root.isFollowRequest) {
+                    i18n("%1 requested to follow you", root.notificationActorIdentity.displayNameHtml)
                 } else {
                     i18n("%1 followed you", root.notificationActorIdentity.displayNameHtml)
                 }
@@ -90,6 +101,14 @@ QQC2.ItemDelegate {
 
             Kirigami.Theme.colorSet: Kirigami.Theme.Window
             Kirigami.Theme.inherit: false
+        }
+
+        QQC2.Button {
+            text: i18nc("@action:button", "Manage Follow Requests")
+            icon.name: "list-add-user"
+            onClicked: (root.QQC2.ApplicationWindow.window as StatefulApp.StatefulWindow)?.application.action("follow_requests").trigger()
+
+            Layout.fillWidth: true
         }
     }
 }

src/content/ui/Notifications/RelationshipSeveranceDelegate.qml
@@ -46,6 +46,7 @@ QQC2.ItemDelegate {
 
         maximumWidth: Kirigami.Units.gridUnit * 40
         spacing: 0
+        padding: 0
 
         RowLayout {
             spacing: Kirigami.Units.smallSpacing

src/content/ui/Notifications/ReportDelegate.qml
@@ -45,6 +45,7 @@ QQC2.ItemDelegate {
 
         maximumWidth: Kirigami.Units.gridUnit * 40
         spacing: 0
+        padding: 0
 
         RowLayout {
             spacing: Kirigami.Units.smallSpacing

src/content/ui/Notifications/UnknownNotificationDelegate.qml
@@ -1,0 +1,54 @@
+// SPDX-FileCopyrightText: 2024 Carl Schwan <carl@carlschwan.eu>
+// SPDX-License-Identifier: LGPL-2.1-or-later
+
+
+import QtQuick
+import QtQuick.Controls as QQC2
+import QtQuick.Layouts
+import Qt.labs.qmlmodels
+import org.kde.kirigami as Kirigami
+import org.kde.tokodon as Tokodon
+
+QQC2.ItemDelegate {
+    id: root
+
+    required property int index
+
+    topPadding: Kirigami.Units.largeSpacing
+    bottomPadding: Kirigami.Units.largeSpacing
+    leftPadding: Kirigami.Units.largeSpacing * 2
+    rightPadding: Kirigami.Units.largeSpacing * 2
+
+    highlighted: false
+    hoverEnabled: false
+
+    width: ListView.view.width
+
+    background: Rectangle {
+        color: Kirigami.Theme.backgroundColor
+
+        Kirigami.Separator {
+            anchors {
+                horizontalCenter: parent.horizontalCenter
+                bottom: parent.bottom
+            }
+
+            width: flexColumn.innerWidth
+        }
+    }
+
+    contentItem: Kirigami.FlexColumn {
+        id: flexColumn
+
+        maximumWidth: Kirigami.Units.gridUnit * 40
+        spacing: 0
+        padding: 0
+
+        QQC2.Label {
+            text: i18nc("@info", "Unknown notification")
+            wrapMode: Text.WordWrap
+
+            Layout.fillWidth: true
+        }
+    }
+}

commit 16572c8df0afb80218b2214615517eaf48eb3735

Fix notification grouping
Fixes a regression I introduced in c75a40e869c8295006cc69544762a5300d431b8d,
it was just an oversight on my part.

src/content/ui/NotificationPage.qml
@@ -20,7 +20,7 @@ Kirigami.ScrollablePage {
     property var dialog: null
 
     property alias listViewHeader: listview.header
-    readonly property bool typesAreGroupable: notificationModel.excludeTypes === []
+    readonly property bool typesAreGroupable: notificationModel.excludeTypes.length === 0
     property bool shouldGroupNotifications: typesAreGroupable
     readonly property var currentModel: shouldGroupNotifications ? groupedNotificationModel : notificationModel
 